59b5f52fd3a36c046c25ac825d278beb18785f46
Yash <yashpatel54257@gmail.com>
2026-09-10 17:11:50 +0530

    updated

--- apps/api/src/config/database.ts
+++ apps/api/src/config/database.ts
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
-
 import { env } from "./env.js";
 
 export async function connectDatabase(): Promise<void> {
-    await mongoose.connect(env.MONGODB_URI);
+    console.log("[DB] connect start, readyState:", mongoose.connection.readyState);
+    console.log("[DB] URI host:", env.MONGODB_URI.split("@")[1]?.split("/")[0]);
 
-    console.log("MongoDB connected");
-}
-
-export async function disconnectDatabase(): Promise<void> {
-    await mongoose.disconnect();
-
-    console.log("MongoDB disconnected");
+    try {
+        await mongoose.connect(env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 5000,
+        });
+        console.log("[DB] connected, readyState:", mongoose.connection.readyState);
+    } catch (err) {
+        console.error("[DB] connect FAILED:", err);
+        throw err;
+    }
 }
